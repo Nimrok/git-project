4787d73a46ee5833bc3027a63c24d1a15afa376c
Nimrok <test_nimrok@mail.ru>
2018-05-30 23:03:04 +0600

    added primal fram code

--- radio-relay/radio-relay.ino
+++ radio-relay/radio-relay.ino
@@ -1,38 +1,70 @@
+/*
+ * 30.05.2018 Для блока радиоуправляемого реле
+ */
+//--------------------------------------------------/
+#include <Wire.h>
+#include <RTC.h>
 #include "nrf24.c"
 #include "nrf24.h"
 #include "nRF24L01.h"
 #include "radioPinFunctions.c"
+//--------------------------------------------------/
+#define relay 7
+RTC time;
+//--------------------------------------------------/
 char radioflag;
 char radio_buf[4];
-char tx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
-char rx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
-
+char tx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7}; //куда отправлять
+char rx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7}; //адрес этого устройства
+//==================================================/
+void write24(int deviceaddress, unsigned int eeaddress, byte data ) 
+{
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.write(data);
+  Wire.endTransmission();
+  delay(5);
+}
+//--------------------------------------------------/ 
+byte read24(int deviceaddress, unsigned int eeaddress ) 
+{
+  byte rdata = 0xFF;
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.endTransmission();
+  Wire.requestFrom(deviceaddress,1);
+  if (Wire.available()) rdata = Wire.read();
+  return rdata;
+}
+//==================================================/
 void setup() 
 {
   Serial.begin(9600);
- nrf24_init();     
- nrf24_config(2,4);
- nrf24_tx_address(tx_address);
- nrf24_rx_address(rx_address);
+  nrf24_init();     
+  nrf24_config(2,4); //второй канал, длина пакета 4 байта
+  nrf24_tx_address(tx_address);
+  nrf24_rx_address(rx_address);
+  time.begin(RTC_DS3231);    //здесь инициализируется i2c шина (F_CPU/2)
+  Wire.begin();
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
+  write24(0x50, 0, 123);
 }
-
+//==================================================/
 void loop() 
 {
-   if(nrf24_dataReady())
-        {
-            nrf24_getData(radio_buf);        
-            Serial.write(radio_buf[0]);
-            Serial.write(radio_buf[1]);
-            Serial.write(radio_buf[2]);
-            Serial.write(radio_buf[3]);
-        radio_buf[0] = 0;
-        radio_buf[1] = 0;
-        radio_buf[2] = 0;
-        radio_buf[3] = 0;
-        Serial.write(radio_buf[0]);
-            Serial.write(radio_buf[1]);
-            Serial.write(radio_buf[2]);
-            Serial.write(radio_buf[3]);
-        }
+  if(nrf24_dataReady())
+  {
+    nrf24_getData(radio_buf);        
+  }
+//--------------------------------------------------/
+  if(millis()%1000==0)
+  {
+    Serial.println(time.gettime("d-m-Y, H:i:s, D"));
+    Serial.print(read24(0x50, 0), DEC); 
+    delay(1);
+  }
 }
-
+//==================================================/
