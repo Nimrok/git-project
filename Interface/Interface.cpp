158b5e70b8dea5fbd95d0a2eaa3fafaf1bdcea35
Alexander <a.kormin@icmotion.me>
2018-06-05 16:22:58 +0600

    hz

--- Interface/Interface.cpp
+++ Interface/Interface.cpp
@@ -24,3 +24,208 @@ char Interface::point(unsigned int timer)
 		return 0;
 	}
 }
+
+menu1.create0(0,str1,3,str11,str12,str13);
+
+menu11.create1(1,str1,2,str11,str12);
+menu12.create1(2,str1,1,str1);
+menu13.create1(3,str1,4,str11,str12,str13,str14);
+
+menu111.create2(4,str1,1,str1);
+menu112.create2(5,str1,1,str1);
+
+menu121.create2(6,str1,1,str1);
+
+menu131.create2(7,str1,1,str1);
+menu132.create2(8,str1,1,str1);
+menu133.create2(9,str1,1,str1);
+menu134.create2(10,str1,1,str1);
+
+void create(char global_num,const char* str1, char element_num,const char* str11,const char* str12,const char* str13,const char* str14)
+{
+	switch (element_num)
+	{
+		case 1:
+		lcd.setCursor(0,0);
+		lcd.print(str1);
+		lcd.setCursor(6,1);
+		lcd.print(str11);
+		break;
+		case 2:
+		lcd.setCursor(0,0);
+		lcd.print(str1);
+		lcd.setCursor(2,1);
+		lcd.print(str11);
+		lcd.setCursor(10,1);
+		lcd.print(str12);
+		break;
+		case 3:
+		lcd.setCursor(0,0);
+		lcd.print(str1);
+		lcd.setCursor(1,1);
+		lcd.print(str11);
+		lcd.setCursor(6,1);
+		lcd.print(str12);
+		lcd.setCursor(11,1);
+		lcd.print(str13);
+		break;
+		case 4:
+		lcd.setCursor(0,0);
+		lcd.print(str1);
+		lcd.setCursor(0,1);
+		lcd.print(str11);
+		lcd.setCursor(4,1);
+		lcd.print(str12);
+		lcd.setCursor(8,1);
+		lcd.print(str13);
+		lcd.setCursor(12,1);
+		lcd.print(str14);
+		break;
+	}
+}
+
+
+void set_arrow(char element_num, char arrow_position)
+{
+  switch (element_num)
+  { 
+    case 1: 
+    break;
+    case 2:
+      lcd.setCursor(5,1);
+      if (arrow_position==0)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(13,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print('<');
+      }
+    break;
+    case 3:
+      lcd.setCursor(4,1);
+      if (arrow_position==0)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(9,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(14,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print('<');
+      }
+    break;
+    case 4:
+    lcd.setCursor(3,1);
+      if (arrow_position==0)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==3)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(7,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==3)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(11,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==3)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(15,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==3)
+      {
+        lcd.print('<');
+      }
+    break;
+  }
+}
+
+
+
+
+
--- radio-relay/radio-relay.ino
+++ radio-relay/radio-relay.ino
@@ -27,6 +27,8 @@ char rx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7}; //адрес этого уст�
 int value = 0;
 char buzz_flag = 0;
 unsigned long buzz_start = 0;
+char display_s = 0;
+char arrow_position = 0;
 //==================================================/
 void write24(int deviceaddress, unsigned int eeaddress, byte data ) 
 {
@@ -82,12 +84,15 @@ void loop()
     value=analogRead(1);
     if ((value > 800) && (value < 830))
     {
-      lcd.setCursor(0,0);
-      lcd.print("----------");
-      lcd.setCursor(0,0);
-      lcd.print("none_btn");
-      lcd.setCursor(0,1);
-      lcd.print(time.gettime("d-m H:i:s"));
+      if (display_s==0)
+      {
+        lcd.setCursor(0,0);
+        lcd.print("----------");
+        lcd.setCursor(0,0);
+        lcd.print("none_btn");
+        lcd.setCursor(0,1);
+        lcd.print(time.gettime("d-m H:i:s"));
+      }
     }
     else if ((value > 750) && (value < 790)){  
       lcd.setCursor(0,0);  
@@ -97,6 +102,8 @@ void loop()
       if (btn_delay.point(300))
       {
         digitalWrite(buzzer,HIGH);
+        display_s = 1;
+        lcd.clear();
         buzz_flag = 1;
         buzz_start=millis();
       }
@@ -111,6 +118,11 @@ void loop()
         digitalWrite(buzzer,HIGH);
         buzz_flag = 1;
         buzz_start=millis();
+        arrow_position--;
+        if (arrow_position<0)
+        {
+          arrow_position=3;
+        }
       }
     }
     else if ((value > 500) && (value < 530)){
@@ -123,6 +135,11 @@ void loop()
         digitalWrite(buzzer,HIGH);
         buzz_flag = 1;
         buzz_start=millis();
+        arrow_position++;
+        if (arrow_position>3)
+        {
+          arrow_position=0;
+        }
       }
     }
     else if (value < 30){
@@ -134,6 +151,8 @@ void loop()
       {
         digitalWrite(buzzer,HIGH);
         buzz_flag = 1;
+        display_s = 0;
+        lcd.clear();
         buzz_start=millis();
       }
     }
@@ -150,7 +169,208 @@ void loop()
     digitalWrite(buzzer, LOW);
   }
 //--------------------------------------------------/
+  if (display_s==1)
+  {
+    create(0,"      MENU      ",4,"El1","El2","El3","El4");
+    set_arrow(4,arrow_position);
+  }
+//--------------------------------------------------/
 }
 //==================================================/
+//
+//
+//Функция принимает количество элементов на экране и позицию стрелки
+//Формирует на экране положение стрелки
+//позицию стрелки задают клавиши left right
+void set_arrow(char element_num, char arrow_position)
+{
+  switch (element_num)
+  { 
+    case 1: 
+    break;
+    case 2:
+      lcd.setCursor(5,1);
+      if (arrow_position==0)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(13,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print('<');
+      }
+    break;
+    case 3:
+      lcd.setCursor(4,1);
+      if (arrow_position==0)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(9,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(14,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print('<');
+      }
+    break;
+    case 4:
+    lcd.setCursor(3,1);
+      if (arrow_position==0)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==3)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(7,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==3)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(11,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print('<');
+      }
+      else if (arrow_position==3)
+      {
+        lcd.print(' ');
+      }
+      lcd.setCursor(15,1);
+      if (arrow_position==0)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==1)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==2)
+      {
+        lcd.print(' ');
+      }
+      else if (arrow_position==3)
+      {
+        lcd.print('<');
+      }
+    break;
+  }
+}
+void create(char global_num,const char* str1, char element_num,const char* str11,const char* str12,const char* str13,const char* str14)
+{
+  switch (element_num)
+  {
+    case 1:
+    lcd.setCursor(0,0);
+    lcd.print(str1);
+    lcd.setCursor(6,1);
+    lcd.print(str11);
+    break;
+    case 2:
+    lcd.setCursor(0,0);
+    lcd.print(str1);
+    lcd.setCursor(2,1);
+    lcd.print(str11);
+    lcd.setCursor(10,1);
+    lcd.print(str12);
+    break;
+    case 3:
+    lcd.setCursor(0,0);
+    lcd.print(str1);
+    lcd.setCursor(1,1);
+    lcd.print(str11);
+    lcd.setCursor(6,1);
+    lcd.print(str12);
+    lcd.setCursor(11,1);
+    lcd.print(str13);
+    break;
+    case 4:
+    lcd.setCursor(0,0);
+    lcd.print(str1);
+    lcd.setCursor(0,1);
+    lcd.print(str11);
+    lcd.setCursor(4,1);
+    lcd.print(str12);
+    lcd.setCursor(8,1);
+    lcd.print(str13);
+    lcd.setCursor(12,1);
+    lcd.print(str14);
+    break;
+  }
+}
+
+
+
+
+
+
+
+
+
 
 
