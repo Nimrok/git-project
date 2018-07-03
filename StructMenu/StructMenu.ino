5594ed1ee880f17706677ecb456d50f1a2dee853
Nimrok <test_nimrok@mail.ru>
2018-07-03 09:11:22 +0600

    new branch to work with menu without others

--- StructMenu/StructMenu.ino
+++ StructMenu/StructMenu.ino
@@ -3,18 +3,10 @@
  */
 //--------------------------------------------------/                   ИНКЛУДЫ
 #include <avr/wdt.h>
-#include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-#include <OneWire.h>
 #include <RTC.h>
-#include "nrf24.c"
-#include "nrf24.h"
-#include "nRF24L01.h"
-#include "radioPinFunctions.c"
 #include <Pointer.h>
 //--------------------------------------------------/                   ДЕФАЙНЫ
-#define light_sensor 8    //вход порогового датчика освещенности
-#define one_wire    7     //вывод интерфейса 1-wire
 #define buzzer      6     //управление активной пищалкой
 #define relay       5     //управление реле
 #define addVar(name, next, prev, symbol, var) extern subvar next;extern subvar prev;subvar name = {(subvar*)&next, (subvar*)&prev, symbol, (void*)&var}
@@ -23,15 +15,11 @@
 //--------------------------------------------------/                   КЛАССОВ ИНИЦИАЛИЗАЦИЯ
 Pointer ask_keyboard;         //точка отсчета опроса клавиатуры
 Pointer button_pushed_time;   //т/о повторного опроса кнопки
-Pointer ask_light_sensor;     //т/о опроса датчика освещенности
-Pointer ds_start_conv;        //т/о запуска преобразования датчика температуры ds18b20
-Pointer ds_get_data;          //т/о запроса данных о температуры от датчика ds18b20
 Pointer draw_menu;            //т/о отрисовки экрана меню
 Pointer draw_workspace;       //т/о отрисовки рабочего экрана
 Pointer draw_element_menu;    //т/о отрисовки экрана элемента меню
 RTC time;
 LiquidCrystal_I2C lcd(0x3F,16,2);
-OneWire ds(one_wire);
 //--------------------------------------------------/                   ПЕРЕМЕННЫЕ
 struct subvar
 {
@@ -55,15 +43,9 @@ struct submenu
 submenu *currentMenu;
 //--------------------------------------------------/
 char relay_state = 0;
-char nrf_data_buffer[4];
-char nrf_destination_buffer[5] = {0xD7,0xD7,0xD7,0xD7,0xD7}; //куда отправлять
-char nrf_id_buffer[5] = {0xE7,0xE7,0xE7,0xE7,0xE7}; //адрес этого устройства
 int analog_button_value = 0;
 char buzzer_started = 0;
 unsigned long buzzer_start_point = 0;
-float ds_temp = 0.0;
-byte ds_low_byte = 0;
-byte ds_high_byte = 0;
 char days=0;
 char months=0;
 char years=0;
@@ -71,7 +53,6 @@ char hours=0;
 char minutes=0;
 char seconds=0;
 char day_number=0;
-addVar(relay_var,relay_var,relay_var,'r',relay_state);
 addVar(Y_var,M_var,d_var,'Y',years);
 addVar(M_var,D_var,Y_var,'M',months);
 addVar(D_var,h_var,M_var,'D',days);
@@ -80,35 +61,12 @@ addVar(m_var,s_var,h_var,'m',minutes);
 addVar(s_var,d_var,m_var,'s',seconds);
 addVar(d_var,Y_var,s_var,'d',day_number);
 addMenu(el1,el2,el3,"Wtch",watch_set_display);
-addMenu(el2,el3,el1,"Rele",relay_control);
-addMenu(el3,el1,el2,"FLSH",none);
+addMenu(el2,el3,el1,"Rele",watch_set_display);
+addMenu(el3,el1,el2,"FLSH",watch_set_display);
 //==================================================/                   ФУНКЦИИ
 void time_set(char days, char months, char years, char hours, char minutes, char seconds, char day_number)
 {
   time.settime(days,months,years,hours,minutes,seconds,day_number);
-}
-//--------------------------------------------------/
-void none()
-{
-  drawout(0,0,"|              |");
-  drawout(0,1,"|              |");
-}
-//--------------------------------------------------/
-void relay_control()
-{
-  lcd.setCursor(0,0);
-  //lcd.print("Relay control   ");
-  lcd.setCursor(0,1);
-  bottom_string_buffer[6] = currentVar->symbol;
-  bottom_string_buffer[0] = currentVar->prev->symbol;
-  bottom_string_buffer[12] = currentVar->next->symbol;  
-  lcd.print(bottom_string_buffer);
-}
-//--------------------------------------------------/
-void drawout(char x, char y, char* str)
-{
-  lcd.setCursor(x,y);
-  lcd.print(str);
 }
 //--------------------------------------------------/
 void watch_set_display()
@@ -116,10 +74,13 @@ void watch_set_display()
   lcd.setCursor(0,0);
   lcd.print("Real time set   ");
   lcd.setCursor(0,1);
-  bottom_string_buffer[6] = currentVar->symbol;
+  bottom_string_buffer[6] = currentVar->symbol;  
   bottom_string_buffer[0] = currentVar->prev->symbol;
   bottom_string_buffer[12] = currentVar->next->symbol;  
   lcd.print(bottom_string_buffer);
+  char value = (char)&currentVar->var;
+  lcd.setCursor(8,1);
+  lcd.print(value,DEC);
 }
 //--------------------------------------------------/
 void moveMenu(submenu *NewMenu)
@@ -131,28 +92,6 @@ void moveVar(subvar *NewVar)
 {
   currentVar = NewVar;
 } 
-//--------------------------------------------------/
-void write24(int deviceaddress, unsigned int eeaddress, byte data ) 
-{
-  Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddress >> 8));   // MSB
-  Wire.write((int)(eeaddress & 0xFF)); // LSB
-  Wire.write(data);
-  Wire.endTransmission();
-  delay(5);
-}
-//--------------------------------------------------/ 
-byte read24(int deviceaddress, unsigned int eeaddress ) 
-{
-  byte rdata = 0xFF;
-  Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddress >> 8));   // MSB
-  Wire.write((int)(eeaddress & 0xFF)); // LSB
-  Wire.endTransmission();
-  Wire.requestFrom(deviceaddress,1);
-  if (Wire.available()) rdata = Wire.read();
-  return rdata;
-}
 //--------------------------------------------------/ 
 void button_active_func()
 {
@@ -296,21 +235,10 @@ void setup()
   Serial.begin(9600);
   lcd.init();                     
   lcd.backlight();
-  nrf24_init();     
-  nrf24_config(2,4); //второй канал, длина пакета 4 байта
-  nrf24_tx_address(nrf_destination_buffer);
-  nrf24_rx_address(nrf_id_buffer);
   time.begin(RTC_DS3231);    //здесь инициализируется i2c шина (F_CPU/2)
   Wire.begin();
   pinMode(relay, OUTPUT);
   digitalWrite(relay, HIGH);
-  pinMode(light_sensor,INPUT);
-  ds.reset();
-  ds.write(0xCC);
-  ds.write(0x4E);
-  ds.write(0x28);
-  ds.write(0x00);
-  ds.write(0x7F);
   time.gettime();
   days=time.day;
   months=time.month;
@@ -328,45 +256,10 @@ void loop()
 //--------------------------------------------------/
   if (menu_opened==0)
   {
-    if(nrf24_dataReady())
-    {
-      nrf24_getData(nrf_data_buffer);        
-    }
-    if (ask_light_sensor.point(30))
-    {
-      if (digitalRead(light_sensor)==0)
-      {
-        digitalWrite(relay, HIGH);
-      }
-      else
-      {
-        digitalWrite(relay, LOW);
-      }
-    }
-    if (ds_start_conv.point(500))
-    {
-      ds.reset();
-      ds.write(0xCC);
-      ds.write(0x44); 
-    }
-    if (ds_get_data.point(1500))
-    {
-      ds.reset();
-      ds.write(0xCC);
-      ds.write(0xBE); 
-      ds_high_byte=ds.read();
-      ds_low_byte=ds.read();
-      ds_temp =  ((ds_low_byte << 8) | ds_high_byte) * 0.0625;
-    }
     if (draw_workspace.point(50))
     {
       lcd.setCursor(0,0);
       lcd.print(time.gettime("d-m, H:i:s"));
-      lcd.setCursor(0,1);
-      lcd.print(ds_temp);
-      lcd.setCursor(13,1);
-      lcd.print(!digitalRead(light_sensor));
-      drawout(5,1,"C light:");
     }
   }
 //--------------------------------------------------/
